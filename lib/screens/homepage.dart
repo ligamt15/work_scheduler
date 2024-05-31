644fe07640b5bef2fdb600bac086396e3138acf4
Vladyslav Filonenko <ligamt15@Gmail.com>
2024-05-31 15:05:34 +0100

    Update homepage.dart

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -97,7 +97,7 @@ class HomePage extends StatelessWidget {
                             'Cant calculate your salary. Check your calendar and next payment date');
                       } else {
                         return Text(
-                            'Salary is: ${snapshot.data?[0]}, Count of working days: ${snapshot.data?[1]}${snapshot.data?[6]}\nProbably salary is: ${snapshot.data?[2]}${snapshot.data?[6]}, Count of all days: ${snapshot.data?[3]}\nTax amount: ${snapshot.data?[4]}\nAnnual salary is: ${snapshot.data?[5]}&{snapshot.data?[6]}');
+                            'Salary is: ${snapshot.data?[0]}, Count of working days: ${snapshot.data?[1]}${snapshot.data?[6]}\nProbably salary is: ${snapshot.data?[2]}${snapshot.data?[6]}, Count of all days: ${snapshot.data?[3]}\nTax amount: ${snapshot.data?[4]}\nAnnual salary is: ${snapshot.data?[5]}${snapshot.data?[6]}');
                       }
                     },
                   )
